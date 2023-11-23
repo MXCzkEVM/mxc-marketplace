3dea9d4e1cfac933bbfbdb14bc93fbdff1e3952c
WangZheng <waangzheeng@gmail.com>
2023-11-23 08:48:45 +0800

    fixed collection

--- webapp/components/CollectionDetail.tsx
+++ webapp/components/CollectionDetail.tsx
@@ -590,6 +590,7 @@ const CollectDetail = (props: any) => {
             </div>
             <div className="inputWrapper">
               <select
+                disabled
                 value={domainValue}
                 onChange={onChangeDomain}
                 className={`block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500`}
