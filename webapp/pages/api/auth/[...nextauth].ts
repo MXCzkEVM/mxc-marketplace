e9511d37d2655051139898ecd574a7167de80423
Mr.Mao <951416545@qq.com>
2023-12-14 02:38:14 +0800

    fix: twitter change name to id

--- webapp/pages/api/auth/[...nextauth].ts
+++ webapp/pages/api/auth/[...nextauth].ts
@@ -1,23 +1,26 @@
 import NextAuth from 'next-auth';
-import TwitterProvider from 'next-auth/providers/twitter';
 
-
-const twitterProvider = TwitterProvider({
-  clientId: process.env.TWITTER_ID!,
-  clientSecret: process.env.TWITTER_SECRET!,
-  version: '2.0',
-})
-twitterProvider.profile = function ({data}: any) {
-  return {
-    id: data.id,
-    name: '-------------------------------------------',
-    email: '-------------------------------------------',
-    image: '-------------------------------------------',
-  }
-}
 export default NextAuth({
   providers: [
-    twitterProvider
+    {
+      id: "twitter",
+      name: "Twitter",
+      type: "oauth",
+      checks: ["pkce", "state"],
+      authorization:
+        "https://twitter.com/i/oauth2/authorize?scope=users.read tweet.read offline.access",
+      token: "https://api.twitter.com/2/oauth2/token",
+      userinfo:
+        "https://api.twitter.com/2/users/me?user.fields=profile_image_url",
+      profile({ data }) {
+        return {
+          id: data.id,
+          name: data.name,
+          email: data.email ?? null,
+          image: data.profile_image_url,
+        }
+      },
+    }
   ],
   callbacks: {
     jwt(jwt) {
