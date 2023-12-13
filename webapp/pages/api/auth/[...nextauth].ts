cf1efb614bb8292a6532fb108a41b93e2ebcda37
Mr.Mao <951416545@qq.com>
2023-12-14 02:05:46 +0800

    fix: twitter change name to id

--- webapp/pages/api/auth/[...nextauth].ts
+++ webapp/pages/api/auth/[...nextauth].ts
@@ -1,32 +1,34 @@
 import NextAuth from 'next-auth';
 import TwitterProvider from 'next-auth/providers/twitter';
 
-const twitterProvider = TwitterProvider({
-  clientId: process.env.TWITTER_ID!,
-  clientSecret: process.env.TWITTER_SECRET!,
-  version: '2.0',
-  authorization: 'https://twitter.com/i/oauth2/authorize?scope=users.read tweet.read offline.access',
-})
 
-twitterProvider.profile = function ({ data }: any) {
-  console.log('profile--------------', data)
-  return {
-    id: data.id || data.id_str,
-    name: data.name,
-    email: data.id || data.id_str || '!!!!!!!!!!!!!!!!!!!',
-    image: data.profile_image_url,
-  }
-}
+
 export default NextAuth({
   providers: [
-    twitterProvider
+    TwitterProvider({
+      clientId: process.env.TWITTER_ID!,
+      clientSecret: process.env.TWITTER_SECRET!,
+      version: '2.0',
+      profile({ data }) {
+        return {
+          id: data.id,
+          name: data.name,
+          email: data.id || 'wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww',
+          image: data.profile_image_url,
+        }
+      },
+    })
   ],
   callbacks: {
-    session({ session }) {
-      if (session.user)
-        session.user.email = '------------------------------------------------'
-      return session
+    jwt(jwt) {
+      console.log('++++++++++++++++++jwt', jwt)
+      return jwt.token
     },
+    session(session) {
+      console.log('------------------session', session)
+      return session.session
+    },
+
   },
   secret: 'e4c972eb07ac063727a910f072f80ab4'
 });
