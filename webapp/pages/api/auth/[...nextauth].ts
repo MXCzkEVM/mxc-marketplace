9ab706757c7da2e9bde901218e27e2bcb248de7b
Mr.Mao <951416545@qq.com>
2023-12-14 00:58:20 +0800

    fix: twitter change name to id

--- webapp/pages/api/auth/[...nextauth].ts
+++ webapp/pages/api/auth/[...nextauth].ts
@@ -7,6 +7,14 @@ export default NextAuth({
       clientId: process.env.TWITTER_ID!,
       clientSecret: process.env.TWITTER_SECRET!,
       version: '2.0',
+      profile(data) {
+        return {
+          id: data.id_str,
+          name: data.screen_name, // this is the Twitter username
+          email: data.email,
+          image: data.profile_image_url_https.replace(/_normal\.(jpg|png|gif)$/, '.$1') as string,
+        };
+      },
     })
   ],
   secret: 'e4c972eb07ac063727a910f072f80ab4'
