b6f8a2933ed2d31a7ffc224a097059474a550a6f
Mr.Mao <951416545@qq.com>
2023-12-13 17:13:06 +0800

    feat: update error

--- webapp/pages/profile/[address].tsx
+++ webapp/pages/profile/[address].tsx
@@ -16,7 +16,6 @@ import { randomColor } from "@/util/randomColor"
 import { zeroAddress } from "@/const/Local"
 import dynamic from "next/dynamic"
 import { useTranslation } from "react-i18next"
-import Modal from "@/components/Modal"
 
 const ProfileCollections = dynamic(
   () => import("@/components/Profile/Collections")
