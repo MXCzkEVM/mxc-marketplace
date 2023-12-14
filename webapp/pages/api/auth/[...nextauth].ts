9eb2213e5e5ce808ab5859db66575a5884a96cf0
Mr.Mao <951416545@qq.com>
2023-12-14 12:03:09 +0800

    chore: custom twitter provider

--- webapp/pages/api/auth/[...nextauth].ts
+++ webapp/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
 
+const USER_INFO_URL = "https://api.twitter.com/2/users/me?user.fields=?user.fields=id,name,username,profile_image_url"
 export default NextAuth({
   providers: [
     {
@@ -10,16 +11,25 @@ export default NextAuth({
       authorization:
         "https://twitter.com/i/oauth2/authorize?scope=users.read tweet.read offline.access",
       token: "https://api.twitter.com/2/oauth2/token",
-      userinfo:
-        "https://api.twitter.com/2/users/me?user.fields=?user.fields=id,name,username,profile_image_url",
+      userinfo: {
+        url: USER_INFO_URL,
+        async request({ tokens }) {
+          const response = await fetch(USER_INFO_URL, {
+            headers: {
+              Authorization: `Bearer ${tokens.access_token}`,
+            },
+          })
+          return response.json()
+        },
+      },
       clientId: process.env.TWITTER_ID!,
       clientSecret: process.env.TWITTER_SECRET!,
       profile({ data }) {
         return {
-          id: '----------------------------------------------',
-          name: '----------------------------------------------',
-          email: '----------------------------------------------' ?? null,
-          image: '----------------------------------------------',
+          id: data.id,
+          name: data.name,
+          email: data.id,
+          image: '--------------------------',
         }
       },
     }
