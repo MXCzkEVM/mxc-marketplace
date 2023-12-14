c17e292b98b5956dd6c0c529ec4c265eca62b5a9
Mr.Mao <951416545@qq.com>
2023-12-14 12:18:19 +0800

    chore: custom twitter provider

--- webapp/pages/api/auth/[...nextauth].ts
+++ webapp/pages/api/auth/[...nextauth].ts
@@ -1,39 +1,35 @@
 import NextAuth from 'next-auth';
+import TwitterProvider from 'next-auth/providers/twitter';
 
-const USER_INFO_URL = "https://api.twitter.com/2/users/me?user.fields=?user.fields=id,name,username,profile_image_url"
+
+
 export default NextAuth({
   providers: [
-    {
-      id: "twitter",
-      name: "Twitter",
-      type: "oauth",
-      checks: ["pkce", "state"],
-      authorization:
-        "https://twitter.com/i/oauth2/authorize?scope=users.read tweet.read offline.access",
-      token: "https://api.twitter.com/2/oauth2/token",
-      userinfo: {
-        url: USER_INFO_URL,
-        async request({ tokens }) {
-          const response = await fetch(USER_INFO_URL, {
-            headers: {
-              Authorization: `Bearer ${tokens.access_token}`,
-            },
-          })
-          const json = await response.json()
-          return {
-            id: json.id,
-            name: json.id,
-            image: '------',
-            email: '------',
-            sub: '-----'
-          }
-        },
-      },
+    TwitterProvider({
       clientId: process.env.TWITTER_ID!,
       clientSecret: process.env.TWITTER_SECRET!,
-      profile: undefined as any
-    }
+      version: '2.0',
+      userinfo: 'https://api.twitter.com/2/users/me?user.fields=id,profile_image_url',
+      profile({ data }) {
+        return {
+          id: data.id,
+          name: data.name,
+          email: data.id,
+          image: data.profile_image_url,
+        }
+      },
+    })
   ],
+  callbacks: {
+    jwt(jwt) {
+      console.log('++++++++++++++++++jwt', jwt)
+      return jwt.token
+    },
+    session(session) {
+      console.log('------------------jwt', session)
+      return session.session
+    },
+  },
   secret: 'e4c972eb07ac063727a910f072f80ab4'
 });
 
