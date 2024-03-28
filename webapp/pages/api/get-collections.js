9db9ae118cbb126c9c733964d57cdcefc5ad1751
Mr.Mao <951416545@qq.com>
2024-03-28 16:13:55 +0800

    chore: update change

--- webapp/pages/api/get-collections.js
+++ webapp/pages/api/get-collections.js
@@ -8,7 +8,29 @@ const redis = new Redis({
   token: process.env.UPSTASH_REDIS_REST_TOKEN,
 })
 
-export default async function handler(req, res) {
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+
+export default allowCors(async function handler(req, res) {
   let { chainId, creator } = req.body
 
   if (!chainId) {
@@ -51,4 +73,4 @@ export default async function handler(req, res) {
       data: { collections: ops },
       message: i18n.t("success")
     })
-}
+})
