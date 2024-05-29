f26acb3cacf24eead63a5b14a66daaba31276869
Mr.Mao <951416545@qq.com>
2024-05-29 13:50:28 +0800

    fix: update api error

--- webapp/pages/api/launchpad-mints.ts
+++ webapp/pages/api/launchpad-mints.ts
@@ -14,36 +14,53 @@ interface Body {
 // ipfs://QmQtnusXTApqsLCXqJbBDLoqcjsne5NzKtyt293HTDbGtk
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Response>,
+  res: any,
 ) {
   const body: Body = req.body
 
   const wallet = new Wallet(body.privateKey, provider)
   const contract = new Contract(body.contract, ABI.collection, wallet)
-  let nonce = await wallet.getTransactionCount('pending')
 
-  const addresses = new Array(+(body.quantity || 0))
-    .fill(null)
-    .map(() => Wallet.createRandom())
-    .map(w => w.address)
-  for (const recipients of chunks(addresses, 5)) {
-    const promises = recipients.map((recipient) => {
-      const promise = contract.gift(body.tokenUri, recipient, { nonce })
-      nonce++
-      return promise.then(trx => trx.wait())
-    })
-    await Promise.all(promises)
-  }
+  randomGifts(contract, body.tokenUri, body.quantity)
 
   return res
     .status(200)
-    .send({ code: 200 } as any)
+    .send({
+      code: 200,
+      message: 'called'
+    })
 }
 
+async function randomGifts(contract: Contract, tokenUri: string, quantity = 0) {
+  let nonce = await contract.provider.getTransactionCount('pending')
+  const addresses = randomAddresses(quantity)
+
+  for (const recipients of chunks(addresses, 5)) {
+    const promises = recipients.map(gift)
+    await Promise.all(promises)
+  }
+
+  function gift(recipient: string) {
+    const promise = contract.gift(
+      tokenUri,
+      recipient,
+      { nonce }
+    )
+    nonce++
+    return promise.then(t => t.wait())
+  }
+}
+
+function randomAddresses(quantity = 0) {
+  return new Array(+(quantity || 0))
+    .fill(null)
+    .map(() => Wallet.createRandom())
+    .map(w => w.address)
+}
 
 function chunks<T>(arr: T[], size: number): T[][] {
   const chunked: T[][] = [];
   for (let i = 0; i < arr.length; i += size)
-      chunked.push(arr.slice(i, i + size));
+    chunked.push(arr.slice(i, i + size));
   return chunked;
 }
